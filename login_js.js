abcd86d78a34cc569ba06a744107dca0f2b4fe8f
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2022-12-03 00:05:07 +0500

    Add files via upload

---
+++ login_js.js
@@ -0,0 +1,12 @@
+// function Refresh(timeoutPeriod) {
+// 	setTimeout("location.reload(true);",timeoutPeriod);
+// }
+// window.onload = Refresh(5000); //reload every 5 second
+
+var type = new Typed(".auto-type",
+	{
+		strings: ["Security Solution", "Protection"],
+		typeSpeed: 50,
+		backSpeed: 50,
+		loop: true
+	})
